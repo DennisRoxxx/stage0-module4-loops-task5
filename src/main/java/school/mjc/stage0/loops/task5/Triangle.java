37e05a1e1b87cd17e8a2a4fd30a3e3e217a92292
Denis <106114126+DennisRoxxx@users.noreply.github.com>
2022-12-15 13:53:21 +0300

    Update Triangle.java

--- src/main/java/school/mjc/stage0/loops/task5/Triangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Triangle.java
@@ -4,7 +4,7 @@ public class Triangle {
     public void printTriangle(int cathetusLength) {
     for (int x = 0; x < cathetusLength; x++) {
             for (int y = 0; y <= x; y++) {
-                System.out.print("8 ");
+                System.out.print("8");
             }
             System.out.println();
         }
